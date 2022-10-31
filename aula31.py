5b4caa306ea79c26e9d42914ff8c06b7b99acaf7
Wendy <wendygaken@gmail.com>
2022-10-31 10:44:56 -0300

    Versão 1

--- aula31.py
+++ aula31.py
@@ -1,17 +1,13 @@
 #test_capitalize
 
-#V3
+#V1
 import pytest
 
 def capital_case(x):
-    if not isinstance(x, str):
-        raise TypeError('Please provide a vaid string argument')
     return x.capitalize()
 
 def test_capital_case():
     assert capital_case('semaphore') == 'Semaphore'
     
-def test_raises_exception_on_non_string_arguments():
-    with pytest.raises(TypeError):
-        capital_case(9)
+
 
